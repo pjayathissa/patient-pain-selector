fa506ff736534b0d42179f1344fa563b9063d047
pjayathissa <p.jayathissa@gmail.com>
2020-06-17 00:22:18 +1200

    added comments

--- js/client.js
+++ js/client.js
@@ -2,11 +2,12 @@
 // better way to do this than a global var
 var uid = 0
 
-
+//Main js function
 function selectPain(e) {
-  console.log("svg clicked")
+
   const humanSvg = document.getElementById("human-svg")
 
+  // determine click position
   if (!e) var e = window.event;
     if (e.pageX || e.pageY)
   {
@@ -20,16 +21,14 @@ function selectPain(e) {
       PosY = e.clientY + document.body.scrollTop
         + document.documentElement.scrollTop;
     }
+
+    //subtract click position from location of the svg in screen
     PosX = PosX - window.scrollX - humanSvg.getBoundingClientRect().left;
     PosY = PosY - window.scrollY - humanSvg.getBoundingClientRect().top;
 
+    // define the marker consisting of a circle and text
     const markerCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
     const markerText = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-
-    markerText.setAttributeNS(null, 'x', PosX - 7.5)
-    markerText.setAttributeNS(null, 'y', PosY + 10)
-    markerText.setAttributeNS(null, 'class', 'marker-text')
-    markerText.setAttributeNS(null, 'id', 'markerText-' + uid.toString())
 
 
     markerCircle.setAttributeNS(null, 'cx', PosX);
@@ -40,28 +39,31 @@ function selectPain(e) {
     markerCircle.setAttributeNS(null, 'fill', "none");
     markerCircle.setAttributeNS(null, 'id', 'markerCircle-' + uid.toString())
 
+
+    //position of text is hacked based on marker radius size. TODO: Make this cleaner
+    markerText.setAttributeNS(null, 'x', PosX - 7.5)
+    markerText.setAttributeNS(null, 'y', PosY + 10)
+    markerText.setAttributeNS(null, 'class', 'marker-text')
+    markerText.setAttributeNS(null, 'id', 'markerText-' + uid.toString())
+
+    //Prmpt for painscore and create text box and assign value to marker
     const painScore = promptPain();
     addTextBox(painScore)
-
-
     const markerTextValue = document.createTextNode(painScore);
 
     markerText.appendChild(markerTextValue)
 
-    console.log(markerCircle)
+
     humanSvg.appendChild(markerCircle)
     humanSvg.appendChild(markerText)
-    console.log(humanSvg)
 
-    console.log(PosX)
-    console.log(PosY)
-    console.log("done")
-
+    //up the unique id by 1 for the next element
     uid +=1
-    console.log(uid)
 
 }
 
+// Alert system prompting for pain
+//This is hacky. TODO: Make a better modal component to deal with this in a cleaner way
 function promptPain(reprompt=false) {
   if(reprompt = true){
     var painScore = prompt("Please just select a number from 0-10");
@@ -78,6 +80,8 @@ function promptPain(reprompt=false) {
   }
 }
 
+//Generate a text box which can be deleted
+//TODO: Add an edit button to modify the pain score without deleting
 function addTextBox(painScore) {
     var myDiv = document.createElement('div');
     myDiv.id = 'divText-' + uid.toString()
@@ -91,14 +95,11 @@ function addTextBox(painScore) {
 }
 
 function deleteInput(e){
-  console.log("deleting input")
-  console.log(Number(e.toElement.id.split("-")[1]))
+  //TODO: Perhaps a cleaner way to do this
   const uidToDelete = Number(e.toElement.id.split("-")[1])
 
   document.getElementById('markerText-' + uidToDelete).remove();
   document.getElementById('markerCircle-' + uidToDelete).remove();
   document.getElementById('divText-' + uidToDelete).remove();
-
-
 }
 
